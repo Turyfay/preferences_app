45d1c66b4e6aba73674d02b85b5f0a70ca2af5c3
David Enamorado <david8enaceituno@gmail.com>
2022-03-20 23:40:20 -0600

    se agrego el drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:preferences_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String routeName = '/home';
+
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+      ),
+      drawer: const SideMenu(),
+      body: const Center(
         child: Text('HomeScreen'),
       ),
     );
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:preferences_app/widgets/widgets.dart';
 
 class SettingsScreen extends StatelessWidget {
+  static const String routeName = '/settings';
   const SettingsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SettingsScreen'),
+      ),
+      drawer: const SideMenu(),
+      body: const Center(
         child: Text('SettingsScreen'),
       ),
     );
